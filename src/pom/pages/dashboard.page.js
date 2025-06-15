565db02164cc028544831b3f5ebe1c30048317f1
Sergio E Bautista M <sebautistam@gmail.com>
2025-06-15 11:27:48 -0500

    Moved sideBar from Dashboard page into Base page, refactored both pages

--- src/pom/pages/dashboard.page.js
+++ src/pom/pages/dashboard.page.js
@@ -1,11 +1,9 @@
-const SideMenuComponent = require ('../components/common/sidemenu.components');
 const BasePage = require('./base.page');
 
 class DashboardPage extends BasePage {
 
     constructor (){
         super('/showcase/angular/appointmentplanner/#/dashboard');
-        this.sideMenu = new SideMenuComponent();
     }
 
 }

7fe71c1b0cc8f8fa071078664cc5078f743a2de2
Sergio E Bautista M <sebautistam@gmail.com>
2025-06-15 11:26:58 -0500

    Moved sideBar from Dashboard page into Base page, refactored both pages

--- src/pom/pages/base.page.js
+++ src/pom/pages/base.page.js
@@ -1,10 +1,12 @@
 const HeaderComponent = require ('../components/common/header.component');
+const SideMenuComponent = require ('../components/common/sidemenu.components');
 
 class BasePage {
 
     constructor (url) {
         this.url = url;
         this.header = new HeaderComponent();
+        this.sideMenu = new SideMenuComponent();
     }
 
     async open () {
